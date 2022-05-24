587b766b6482bf160def383e26732febabcec481
sherryyu <bambooyu715@gmail.com>
2022-05-24 10:30:49 -0400

    04

--- problems/04-range.js
+++ problems/04-range.js
@@ -12,7 +12,21 @@ range(7, 6); // []
 
 
 // your code here
+function range(num1, num2) {
+  if ((num2 < num1) || (num1 >= num2)) return [];
+  return [...[num1], ...range((num1+1), num2)]
+}
+ // [1, 2, 3, 4]
+console.log(range(1, 5));
+// [...[1], ...range(2, 5)]
+// [...[2], ...range((3, 5)]
+// [...[3], ...range(4, 5)]
+// [...[4], ...range(5, 5)] return
 
+// [4]
+// [3,4]
+// [2,3,4]
+// [1,2,3,4]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
