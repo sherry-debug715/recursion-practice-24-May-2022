109a73b32da603b87c118523793a42630bcf2cd5
sherryyu <bambooyu715@gmail.com>
2022-05-24 10:57:05 -0400

    solved 06

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -13,6 +13,14 @@ addToTwelve([1]); // false
 ***********************************************************************/
 
 // your code here
+function addToTwelve(arr) {
+  if (arr.length === 1) return false
+  if(arr[0] + arr[1] === 12) return true
+  return addToTwelve(arr.slice(1))
+}
+
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
