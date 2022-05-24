611c4eb23b0bfd14ae7d13106dbc6f8639ce983f
sherryyu <bambooyu715@gmail.com>
2022-05-24 10:48:28 -0400

    solved 05

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -13,7 +13,25 @@ reverse(""); // ""
 ***********************************************************************/
 
 // your code here
+function reverse(str) {
+  let strArr = str.split('')
+  if (!strArr.length) return '';
+  let lastIndex = strArr.length - 1
+  return strArr[lastIndex]  += reverse(strArr.slice(0,lastIndex).join('')) 
 
+}
+
+console.log(reverse("house")); // "esuoh"
+// e += reverse('hous')
+// s += reverse('hou')
+// u += reverse('ho')
+// o += reverse('h')
+// h += null return
+// h
+// oh
+// uoh
+// suoh
+// esuoh
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
